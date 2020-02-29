ef69625937683804cb242b0b107a99ec4cb78b6b
Auraofdivinity <peirisasel373@gmail.com>
2020-02-29 13:22:44 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,149 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import CSVReader from 'react-csv-reader';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      initialData: {},
+      filterOneDisplayed: 'none',
+      filterOneContent: [],
+      filterTwoDisplayed: 'none',
+      filterTwoContent: [],
+      answerDisplayed: 'none',
+      answerContent: []
+    };
+  }
+
+  initiateData = data => {
+    this.setState({ initialData: Array.from(data) });
+    this.handleFilterOne();
+  };
+
+  handleFilterOne = () => {
+    let data = this.state.initialData;
+    this.setState({
+      filterOneContent: data[0].slice(1),
+      filterOneDisplayed: 'inline'
+    });
+  };
+
+  handleFilterOneClick = fieldName => {
+    let data = this.state.initialData;
+    let selectedData = [];
+    let i = data[0].indexOf(fieldName);
+
+    data = data.slice(1);
+
+    data.forEach(element => {
+      selectedData.push(element[i]);
+    });
+
+    this.setState({
+      filterTwoContent: Array.from(new Set(selectedData)),
+      filterTwoDisplayed: 'inline',
+      selectedFieldIndex: i
+    });
+  };
+
+  handleFilterTwoClick = valueName => {
+    let answers = [];
+    let data = this.state.initialData.slice(1);
+    let i = this.state.selectedFieldIndex;
+
+    data.forEach(element => {
+      if (element[i] === valueName) {
+        answers.push(element[0]);
+      }
+    });
+
+    this.setState({
+      answerContent: answers,
+      answerDisplayed: 'inline'
+    });
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <div className='container'>
+          <div
+            style={{
+              width: '70%',
+              height: '100%',
+              margin: '0 auto',
+              border: '0.5px solid black',
+              borderRadius: '10px',
+              borderColor: '#d6d3c9',
+              padding: '10px'
+            }}
+          >
+            <div className='d-flex flex-column h-100 '>
+              <div className='flex-fill box'>
+                <CSVReader onFileLoaded={data => this.initiateData(data)} />
+              </div>
+              <div
+                className='flex-fill box'
+                style={{ display: this.state.filterOneDisplayed }}
+              >
+                <h5>What field do you want to filter by?</h5>
+                {this.state.filterOneContent.map((current, index) => {
+                  return (
+                    <Button
+                      variant='primary'
+                      key={index}
+                      style={{ margin: '10px' }}
+                      onClick={() => this.handleFilterOneClick(current)}
+                    >
+                      {current}
+                    </Button>
+                  );
+                })}
+              </div>
+              <div
+                className='flex-fill box'
+                style={{ display: this.state.filterTwoDisplayed }}
+              >
+                <h5>Select What value you want to filter on?</h5>
+                {this.state.filterTwoContent.map((current, index) => {
+                  return (
+                    <Button
+                      variant='primary'
+                      key={index}
+                      style={{ margin: '10px' }}
+                      onClick={() => this.handleFilterTwoClick(current)}
+                    >
+                      {current}
+                    </Button>
+                  );
+                })}
+              </div>
+              <div
+                className='flex-fill box'
+                style={{ display: this.state.answerDisplayed }}
+              >
+                {this.state.answerContent.map((current, index) => {
+                  return (
+                    <Button
+                      variant='success'
+                      key={index}
+                      style={{ margin: '10px' }}
+                    >
+                      {current}
+                    </Button>
+                  );
+                })}
+                <p>Refresh to select again</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
